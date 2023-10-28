9500e0779c90704806acae5864bdecd9c889c6f6
Svetlana <svetailina1987@gmail.com>
2023-10-28 14:54:44 +0400

    feat: add choose page

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
+
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach((item) => {
+  item.addEventListener('click', open)
+})
+
+
+function open(e) {
+ 
+  tabItem.forEach(i => i.classList.remove('tabs__btn-item--active'));
+  e.target.classList.add('tabs__btn-item--active');
+  ;
+
+  tabContent.forEach(i => i.classList.remove('tabs__content-item--active'));
+
+  document.querySelector(`#${e.target.getAttribute('data-button')}`).classList.add('tabs__content-item--active')
+ 
+}
+
+
 const swiper = new Swiper('.swiper', {
    
-    direction: 'horizontal',
-    loop: true,
-    allowTouchMove: false,
-    effect: "fade",
-    
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
+  direction: 'horizontal',
+  loop: true,
+  allowTouchMove: false,
+  effect: "fade",
+  
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
 
-    autoplay:{
+  autoplay:{
 delay: 3000,
 disableOnInteraction: false,
-    }
-  
-    
-  
-   
-  });
+  }
 
+  
+
+ 
+});
 
